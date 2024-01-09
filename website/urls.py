2dd1cd725a5b75a47ee5341f90041b7c3aa29ac2
brandon23567 <brandonmahuvava0@gmail.com>
2024-01-09 19:19:16 +0530

    still on the statis files

--- website/urls.py
+++ website/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("base.urls")),
     path("authentication/", include("authentication.urls")),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
