5c7fd7e80cf306950729b2d2ebbbca07c7e48d47
brandon23567 <brandonmahuvava0@gmail.com>
2024-01-09 18:01:25 +0530

    i forgot the requirements file

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("base.urls")),
+    path("/", include("base.urls")),
     path("authentication/", include("authentication.urls")),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
